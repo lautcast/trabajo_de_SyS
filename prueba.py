d651b969025c1720f017fd28bfb8d086b01e7b60
Lautaro <12caslautaro@gmail.com>
2026-04-18 13:18:24 -0300

    segundo cambio

--- prueba.py
+++ prueba.py
@@ -1,3 +1,9 @@
 nombre  = "Laucha"
 
 print(f"Mi nombre es {nombre}.")
+
+def elevador(base:int, exponente:float) -> float:
+    calculo = base**exponente
+    return calculo
+
+print(f"Tengo: {elevador(4,5.2)}")
