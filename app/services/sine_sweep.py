055e1ff30eb6217ef1dbbf07c5bf226e57483016
Lautaro <12caslautaro@gmail.com>
2026-05-06 10:44:26 -0300

    fix: extension de los docstrings en el codigo de la funcion del sine sweep logaritmico

--- app/services/sine_sweep.py
+++ app/services/sine_sweep.py
@@ -7,6 +7,7 @@ import numpy as np
 
 
 def generar_sine_sweep(f1: float = 20.00, f2: float = 20000.00, duracion: float = 2, fs: int = 48000) -> tuple[np.ndarray, np.ndarray]:
+    
     """
     Acciones
     --------
@@ -18,10 +19,10 @@ def generar_sine_sweep(f1: float = 20.00, f2: float = 20000.00, duracion: float 
 
     Parametros
     ----------
-    f1 : float  -->  Frecuencia inicial del barrido en Hz (tipicamente 20 Hz).
-    f2 : float  -->  Frecuencia final del barrido en Hz (tipicamente 20000 Hz).
-    duracion : float  -->  Duracion del barrido en segundos.
-    fs : int  -->  Frecuencia de muestreo en Hz.
+    f1 : float  -->  Frecuencia inicial del barrido en Hz (predeterminado: 20 Hz).
+    f2 : float  -->  Frecuencia final del barrido en Hz (predeterminado: 20000 Hz).
+    duracion : float  -->  Duracion del barrido en segundos (predeterminado: 2 segundos).
+    fs : int  -->  Frecuencia de muestreo en Hz (predeterminado: 48000 Hz).
 
     Returns
     -------
@@ -33,23 +34,33 @@ def generar_sine_sweep(f1: float = 20.00, f2: float = 20000.00, duracion: float 
     .. [1] Farina, A. (2000). "Simultaneous measurement of impulse response and distortion with a swept-sine technique."
     """
 
-    # Creamos un arreglo con  N = (duracion * fs) muestras.
-    # Dividimos por fs para obtener tiempos reales en lugar de muestras.
-    t = np.arange(int(duracion * fs)) / fs
+    # Creamos un arreglo con  N = (duracion * fs) muestras, y armamos un arreglo con esa cantidad de muestras. 
+    # Luego, dividimos por la frecuencia de muestreo para obtener tiempos reales en lugar de muestras, y armar el vector tiempo.
 
-    # Construimos la fase 
+    muestras = duracion * fs
+    t = np.arange(int(muestras)) / fs
+
+    # Construimos la fase del sine sweep senoidal a partir de la tecnia Farina.
+    
     fase = (2 * np.pi * f1 * duracion / (np.log(f2 / f1))) * (np.exp((t / duracion) * (np.log(f2 / f1))) - 1)
+    
+    # Creamos el sine sweep a partir de dicha fase.
     
     sweep = np.sin(fase)
 
-    # 3. Construir el filtro inverso
-    # Invertimos el arreglo del sweep en el tiempo usando slicing
+    # Ahora, para construir el filtro inverso, primero invertimos el arreglo del sine sweep en el tiempo usando la funcion de slicing
+    
     sweep_invertido = sweep[::-1]
 
-    # Creamos la envolvente de atenuación para "blanquear" el espectro del filtro
+    # El sine sweep tiene mucha energía acumulada en los graves y poca en los agudos, por lo que queremos que el filtro inverso compense esta situacion.
+    # Para ello, creamos una envolvente que depende del vector tiempo, la cual atenúa las frecuencias bajas y amplifica las altas.
     envolvente = (f2 / f1) ** -(t / duracion)
     
+    # Finalmente multiplicamos la envolvente con el sine sweep invertido para obtener el filtro inverso.
+
     filtro_inverso = sweep_invertido * envolvente
+
+    # La función creada devuelve tanto el sine sweep logarítmico como su filtro inverso. Ambos valores son arreglos.
 
     return sweep, filtro_inverso
 
