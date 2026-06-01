8188eabbc46bbd7e586c37a9a9a7fdd53a5cbba1
Lautaro <12caslautaro@gmail.com>
2026-06-01 11:25:29 -0300

    feat: primera actualizacion del codigo de filtro_octava

--- app/services/filter.py
+++ app/services/filter.py
@@ -4,12 +4,15 @@ Milestone 2: Procesamiento de la respuesta al impulso.
 """
 
 import numpy as np
+from scipy import signal
 
 
-def filtro_octava(
-    signal: np.ndarray, fc: float, fs: int, orden: int = 4
-) -> np.ndarray:
-    """Aplica un filtro pasabanda de una octava centrado en ``fc``.
+def filtro_octava(signal: np.ndarray, fc: float, fs: int, orden: int = 4) -> np.ndarray:
+    
+    """
+    Acciones
+    --------
+    Aplica un filtro pasabanda de una octava centrado en ``fc``.
 
     Implementa un filtro Butterworth pasabanda cuyas frecuencias de corte
     corresponden a los limites de una banda de octava segun IEC 61260:
@@ -18,18 +21,41 @@ def filtro_octava(
 
     Parameters
     ----------
-    signal : np.ndarray
-        Senal de entrada (array 1D).
-    fc : float
-        Frecuencia central de la banda de octava en Hz.
-    fs : int
-        Frecuencia de muestreo en Hz.
-    orden : int, optional
-        Orden del filtro Butterworth (por defecto 4).
+    signal: np.ndarray --> Senal de entrada (array 1D).
+    fc: float --> Frecuencia central de la banda de octava en Hz.
+    fs: int --> Frecuencia de muestreo en Hz.
+    orden: int, optional --> Orden del filtro Butterworth (por defecto 4).
 
     Returns
     -------
-    np.ndarray
-        Senal filtrada (array 1D).
+    np.ndarray --> Senal filtrada (array 1D).
     """
-    raise NotImplementedError("Implementar en Milestone 2")
+
+    # Aseguramos que la frecuencia central entregada esté dentro de las normalizadas.
+
+    frec_cent_normalizadas = np.array[31.5, 63, 125, 250, 500, 1000, 2000, 4000, 8000, 16000]
+
+    if fc not in frec_cent_normalizadas:
+        raise(ValueError("Por favor, ingresar una frecuencia central normalizada según IEC 61620."))
+
+    # Calculamos cada una de las frecuencias de corte por cada frecuencia central normalizada.
+
+    f_inf = fc/(np.sqrt(2))
+    f_sup = fc*(np.sqrt(2))
+
+    # Calculamos la frecuencia de Nyquist
+
+    f_nyquist = fs/2
+
+    # Ahora dividimos cada una de las frecuencias de corte con la frecuencia de sampleo fs, para cumplir con el teorema de Nyquist
+
+    w_inf = f_inf/f_nyquist
+    w_sup = f_sup/f_nyquist
+
+    # Calculamos los parámetros del filtro butterworth
+
+    b, a = signal.butter(orden, [w_inf, w_sup], btype='band')
+
+    signal_filtrada = signal.filtfilt(b, a, signal)
+
+    return signal_filtrada
